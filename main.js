2af84446e778ab28b3041e6f4e8bce3bd7d3343e
Chandan0424 <76838260+Chandan0424@users.noreply.github.com>
2021-01-04 17:49:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function Add(){
+    var num1=Number(document.getElementById("firstno").value);
+    var num2=Number(document.getElementById("secondno").value);
+    var AddResult=num1+num2;
+    window.alert(AddResult);
+}
+function Sub(){
+    var num1=Number(document.getElementById("firstno").value);
+    var num2=Number(document.getElementById("secondno").value);
+    var SubResult=num1-num2;
+    window.alert(SubResult);
+}
+function Multiply(){
+    var num1=Number(document.getElementById("firstno").value);
+    var num2=Number(document.getElementById("secondno").value);
+    var MultiplyResult=num1*num2;
+    window.alert(MultiplyResult);
+}
+function Div(){
+    var num1=Number(document.getElementById("firstno").value);
+    var num2=Number(document.getElementById("secondno").value);
+    var divResult=num1/num2;
+    window.alert(divResult);
+}
